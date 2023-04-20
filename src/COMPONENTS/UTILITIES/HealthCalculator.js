21aaeb4878446327c46b49ba05fe6b4214fd9350
happycode-git <happycode.inbox@hotmail.com>
2023-04-19 21:09:40 -0700

    Apr192023v2

--- src/COMPONENTS/UTILITIES/HealthCalculator.js
+++ src/COMPONENTS/UTILITIES/HealthCalculator.js
@@ -89,8 +89,8 @@ export default function Webline() {
                 "Compatibility: The website should be compatible with different browsers and devices, ensuring that all users can access and use the website regardless of their preferred browser or device. This includes considering differences in screen sizes, operating systems, and browser versions when designing and testing website functionality.",
                 "Security: The website should be secure, with appropriate measures in place to protect user data and prevent unauthorized access. This includes using SSL encryption, implementing strong password policies, and regularly updating and maintaining the website's software and plugins.",
                 "Accessibility: The website should be accessible to all users, including those with disabilities or impairments. This includes complying with accessibility guidelines, such as providing alternative text for images, using descriptive link text, and ensuring that all functionality is available through keyboard navigation."]
-        }
-        , {
+        },
+        {
             id: 7,
             Name: "Originality",
             Desc: "Originality is relevant for websites as it enables them to stand out from the competition, creating a lasting impression on users. A unique website can establish brand identity, and enhance user engagement and time spent on the site.",
@@ -103,6 +103,133 @@ export default function Webline() {
         }
     ]
     const [compID, setCompID] = useState(-1)
+    // 
+    const scorePoints = [
+        {
+            id: 0,
+            Comp: "SEO",
+            Desc: "SEO (Search Engine Optimization) helps websites by enhancing their visibility and ranking on search engine results pages, which can increase the probability of users clicking through to the site. This, in turn, can lead to more traffic, higher conversion rates, and improved business success.",
+            Points: [
+                [1, "The website has poor search engine optimization (SEO) with missing meta tags, low-quality content, and lack of proper keyword usage."],
+                [2, "Although some basic SEO elements are in place, such as meta tags and keyword usage, the website lacks proper optimization for search engines, resulting in low visibility in search results."],
+                [3, "The website has decent SEO with relevant meta tags, optimized content, and moderate keyword usage, but there is room for improvement in terms of optimizing for search engines."],
+                [4, "The website has good SEO practices in place, with optimized meta tags, high-quality content, and proper keyword usage, resulting in improved search engine rankings and visibility."],
+                [5, "The website has excellent SEO, with thorough optimization of meta tags, high-quality content, proper keyword usage, and effective backlinking, resulting in top rankings in search engine results and high visibility to target audiences."]
+            ]
+        },
+        {
+            id: 1,
+            Comp: "Design",
+            Desc: "Design helps websites create visually appealing and engaging user experiences, which can increase user engagement, time spent on site, and ultimately, conversion rates. A well-designed website also conveys professionalism and credibility, helping to establish trust and loyalty with users.",
+            Points: [
+                [1, "The website lacks clear navigation and the layout is cluttered and confusing."],
+                [2, "Although the website has clear navigation, the design is outdated and unappealing."],
+                [3, "The website has a clean and modern design, but there are a few usability issues that need to be addressed."],
+                [4, "The website has an excellent user interface, with intuitive navigation and visually appealing design."],
+                [5, "The website is a masterclass in design, with a seamless user experience and stunning visual elements that enhance the brand's message."]
+            ]
+        },
+        {
+            id: 2,
+            Comp: "UX",
+            Desc: "UX (User Experience) focuses on creating a positive and seamless experience for users, resulting in higher user satisfaction, engagement, and loyalty. Additionally, good UX design can minimize user frustration, improve site navigation, and increase the chances of users returning to the site in the future.",
+            Points: [
+                [1, "The website has a poor user experience (UX) with confusing navigation, slow loading speed, and non-responsive design."],
+                [2, "Although some basic UX elements are in place, such as clear navigation and responsive design, the website lacks advanced features to enhance user experience, resulting in an average user experience."],
+                [3, "The website has decent UX with clear navigation, responsive design, and some advanced features, but there is room for improvement to provide a more engaging and personalized user experience."],
+                [4, "The website has good UX practices in place, with clear navigation, fast loading speed, responsive design, and advanced features, resulting in an improved user experience and increased engagement."],
+                [5, "The website has an excellent user experience, with intuitive navigation, fast loading speed, responsive design, and advanced features that enhance the user journey and provide a personalized experience, resulting in high user satisfaction and increased conversions."]
+            ]
+        },
+        {
+            id: 3,
+            Comp: "Photos",
+            Desc: "Photos can add value to websites by visually conveying information, telling stories, and eliciting emotions that may not be possible with text alone. Additionally, images can break up lengthy paragraphs of text, improve the website's aesthetic appeal, and enhance user engagement and duration of stay on the site.",
+            Points: [
+                [1, "The website has poor quality photos with low resolution, pixelation, and poor lighting."],
+                [2, "Although some photos are of decent quality, others lack consistency and do not effectively convey the brand's message."],
+                [3, "The website has a mix of high and low-quality photos, with some images enhancing the brand message, while others lack consistency in style and tone."],
+                [4, "The website has high-quality photos that effectively showcase the brand's message, with consistent style and tone, resulting in an engaging visual experience for users."],
+                [5, "The website has stunning, high-quality photos that effectively convey the brand's message, with consistent style and tone, resulting in an exceptional visual experience that enhances user engagement and brand perception."]
+            ]
+        },
+        {
+            id: 4,
+            Comp: "Menu",
+            Desc: "A website's menu plays a crucial role in providing users with a structured and coherent path to navigate through its pages, facilitating their search for specific information. Additionally, a well-crafted menu can significantly enhance the user experience by minimizing user confusion and frustration, making the site more accessible, and ultimately increasing the chances of users revisiting the website.",
+            Points: [
+                [1, "The website has a poor menu design, with cluttered and confusing navigation, making it difficult for users to find what they need."],
+                [2, "Although the menu is somewhat organized, there are too many submenus and dropdowns, making it overwhelming for users to navigate."],
+                [3, "The website has a decent menu design, with clear organization and easily identifiable categories, but there is room for improvement in terms of simplifying the navigation."],
+                [4, "The website has a good menu design, with clear and concise navigation, making it easy for users to find what they need quickly and efficiently."],
+                [5, "The website has an excellent menu design, with intuitive and streamlined navigation, providing users with an exceptional browsing experience that enhances engagement and encourages exploration."]
+            ]
+        },
+        {
+            id: 5,
+            Comp: "Color Palette",
+            Desc: "A color palette aids in establishing brand identity and visual style, making the website more memorable and recognizable to users. Additionally, a well-designed color palette can create a cohesive and harmonious visual experience, improving the overall aesthetics of the site and increasing user engagement.",
+            Points: [
+                [1, "Website's poor color palette clashes and lacks consistency, leading to an unappealing and unreadable appearance."],
+                [2, "The website's colors have some consistency but lack contrast, causing a lack of hierarchy and difficulty in focusing on key elements."],
+                [3, "The website's color palette is decent, with complementary colors but needs improvement in contrast and hierarchy."],
+                [4, "The website has a good color palette, with consistent colors that create a strong visual hierarchy, improving user experience."],
+                [5, "The website's excellent color palette thoughtfully enhances user experience and effectively communicates the brand's message, resulting in an engaging and visually appealing site."]
+            ]
+        },
+        {
+            id: 6,
+            Comp: "Functionality",
+            Desc: "Functionality in websites ensures the website operates effectively and meets the needs of its users. When a website functions smoothly and with ease, it can enhance the user experience, increase user satisfaction, and ultimately lead to users returning to the website in the future.",
+            Points: [
+                [1, "Poor website functionality due to broken links, slow load times, and error messages."],
+                [2, "Inconsistent functionality due to broken links and technical issues, resulting in an unreliable user experience."],
+                [3, "Decent functionality, but needs improvement in speed and ease of use."],
+                [4, "Good functionality with all features working efficiently, creating a user-friendly experience."],
+                [5, "Excellent functionality with a seamless user experience, ensuring all features work smoothly, improving user satisfaction."]
+            ]
+        },
+        {
+            id: 7,
+            Comp: "Originality",
+            Desc: "Originality is relevant for websites as it enables them to stand out from the competition, creating a lasting impression on users. A unique website can establish brand identity, and enhance user engagement and time spent on the site.",
+            Points: [
+                [1, "The website lacks originality, with overused templates, cliched designs, and no unique features, making it difficult to distinguish from competitors."],
+                [2, "Although some unique features are present, the website relies heavily on overused templates and design cliches, resulting in a lack of creativity and originality."],
+                [3, "The website has some original elements, such as unique designs and features, but there is room for improvement in terms of standing out from competitors."],
+                [4, "The website has good originality, with creative and innovative designs and features that set it apart from competitors."],
+                [5, "The website is highly original, with a unique and distinctive design and features that are unlike any others in the industry, resulting in a memorable and exceptional user experience."]
+            ]
+        }
+    ]
+    // 
+    const calculateScore = () => {
+        const allSelects = document.querySelectorAll('.ddQuizScore')
+        var total = 0
+        var things = []
+        for (var i = 0; i < allSelects.length; i = i + 1) {
+            const select = allSelects[i].value
+            things.push(select)
+        }
+        var string = ""
+
+        if (!things.includes("- Select one -")) {
+            for (var i = 0; i < things.length; i = i + 1) {
+                const num = parseInt(things[i].split('.')[0])
+                total += num
+                const text = scorePoints[i].Points[num - 1][1]
+                string += text + " "
+            }
+            setDetails(string)
+    
+            // 
+            const tot = total / 40
+            setTotalScore(tot)
+        }
+    }
+
+    const [details, setDetails] = useState("")
+    const [totalScore, setTotalScore] = useState(0)
 
     useEffect(() => {
         closeNav()
@@ -153,24 +280,54 @@ export default function Webline() {
                 </div>
 
             </div>
+            <div className='divider'></div>
+            <h1 className='quiz-large-head padding'>
+                What condition is your website in? <span className='yellow'>Let's find out!</span>
+            </h1>
 
-            <div className='webline-panel2'>
-                <h2>
-                    What condition is your website in? <span className='yellow'>Let's find out!</span>
-                </h2>
-            </div>
-
-            <div className='calc'>
-                <h4 className='calc-head'>Grade the site as accurately as possible based on the components above. The score will be revealed at the bottom of the scoring sheet.</h4>
-                <label>SEO</label>
-                <br/>
-                <select id="ddSEO">
-                    <option>1 (Lost Cause)</option>
-                    <option>2 (Needs Improvement)</option>
-                    <option>3 (Average)</option>
-                    <option>4 (Good)</option>
-                    <option>5 (Impeccable)</option>
-                </select>
+            <div className='quiz'>
+                <h4 className='quiz-head'>Grade the site as accurately as possible based on the components above. The score will be revealed at the bottom of the scoring sheet.</h4>
+                <br />
+                <div className='quiz-wrap'>
+                    {
+                        scorePoints.map((thing, i) => {
+                            return (
+                                <div key={i} className='quiz-block'>
+                                    <h4>Rate your site based on <b className='yellow'>{thing.Comp}</b>.</h4>
+                                    <p>{thing.Desc}</p>
+                                    <br />
+                                    <div className='quiz-options'>
+                                        <select className="ddQuizScore">
+                                            <option>- Select one-</option>
+                                            {
+                                                thing.Points.map((point, j) => {
+                                                    return (
+                                                        <option key={j}>{point[0]}. {
+                                                            point[0] == 1 ? "Not Good" :
+                                                                point[0] == 2 ? "Needs Improvement" :
+                                                                    point[0] == 3 ? "Average" :
+                                                                        point[0] == 4 ? "Good" :
+                                                                            point[0] == 5 ? "Fantastic!" : ""
+                                                        }</option>
+                                                    )
+                                                })
+                                            }
+                                        </select>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <br />
+                <button className='quiz-btn' onClick={calculateScore}>Calculate!</button>
+                {
+                    details != "" ?
+                        <div className='score'>
+                            <h1>This site scored: <span className='yellow'>{(totalScore * 100).toFixed(1)}%</span> </h1>
+                            <p>{details}</p>
+                        </div> : <div></div>
+                }
             </div>
 
             <Footer />
